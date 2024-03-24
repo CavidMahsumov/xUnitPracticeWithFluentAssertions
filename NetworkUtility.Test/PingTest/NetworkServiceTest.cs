dfdf7ecc0cbef8d44b80a4e16fe94a4c6c00f5dc
Cavid <mehsumovcavid@gmail.com>
2024-03-24 22:29:43 +0400

    Add project files.

---
+++ NetworkUtility.Test/PingTest/NetworkServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Test.PingTest
+{
+    public class NetworkServiceTest
+    {
+        public NetworkService _networkService { get; set; }
+        public NetworkServiceTest()
+        {
+            _networkService = new NetworkService(); 
+        }
+
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            var result= _networkService.SendPing();
+
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success:Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        public void NetworkService_PingTime_ReturnInt(int a,int b,int expected)
+        {
+            var result=_networkService.PingTime(a,b);
+
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-10000, 0);
+
+
+        }
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            return "Success:Ping Sent!";
+        }
+        public int PingTime(int num1,int num2) 
+        {
+            return num1+num2;
+        }
+    }
+}
